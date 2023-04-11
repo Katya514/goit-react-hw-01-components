89784509a2ed08b4ce47426732b50793f8081295
Katya514 <rutkovskaaa@gmail.com>
2023-04-10 20:51:36 -0600

    commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import {
+  ProfileCard,
+  Description,
+  Image,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatsItem,
+  Label,
+  Quantity,
+} from './ProfileStyles';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <ProfileCard>
+      <Description>
+        <Image src={avatar} alt="User avatar" />
+        <Name> {username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </ProfileCard>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
---
+++ src/components/ProfileStyles.jsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+
+export const ProfileCard = styled.div`
+text-align: center;
+padding-top: 20px;
+width: 350px;
+border-radius: 4px;
+box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+`;
+
+export const Description = styled.div``;
+
+export const Image = styled.img`
+  width: 50%;
+  border-radius: 50%;
+`;
+export const Name = styled.p`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: 700;
+`;
+export const Tag = styled.p`
+  margin-top: 0;
+  margin-bottom: 10px;
+  font-weight: 500;
+  color: gray;
+`;
+export const Location = styled.p`
+  margin-top: 0;
+  margin-bottom: 30px;
+  font-weight: 500;
+  color: gray;
+`;
+
+export const Stats = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: stretch;
+  margin: 0;
+  padding: 0;
+  height: 100px;
+  list-style: none;
+  background-color: #ecebeb
+  ;
+`;
+
+export const StatsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 33.33%;
+  border-top: 1px solid #c3c8a4;
+  border-right: 1px solid #c3c8a4;
+`;
+export const Label = styled.span`
+  font-weight: 500;
+  color: grey;
+  margin-bottom:5px;
+`;
+export const Quantity = styled.span`
+  font-weight: 700;
+  font-size: 18px;
+`;
